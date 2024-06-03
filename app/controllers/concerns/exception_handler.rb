92828323fd31205c893b9a219893342510ed5994
woohyukjeong <woohyukjeong@dramancompany.com>
2024-06-03 16:29:07 +0900

    커스텀 예외처리 핸들러 수정 - json response 메서드 통일

--- app/controllers/concerns/exception_handler.rb
+++ app/controllers/concerns/exception_handler.rb
@@ -1,18 +1,38 @@
 module ExceptionHandler
   extend ActiveSupport::Concern
 
+  # 인증관련 에러 서브 클래스 정의
+  class InvalidToken < StandardError; end
+  class MissingToken < StandardError; end
+  class AuthenticationError < StandardError; end
+
   included do
     rescue_from ActiveRecord::RecordNotFound do |e|
-      json_response({ message: e.message}, :not_found)
+      err_response(e.message, :not_found)
     end
 
     rescue_from ActiveRecord::RecordInvalid do |e|
-      json_response({ message: e.message}, :unprocessable_entity)
+      err_response(e.message, :unprocessable_entity)
     end
 
     rescue_from ActionController::ParameterMissing do |e|
-      json_response({ message: e.message}, :bad_request)
-
+      err_response(e.message, :bad_request)
     end
+
+    rescue_from ExceptionHandler::InvalidToken, with: :unauthorized_request
+    rescue_from ExceptionHandler::MissingToken, with: :unauthorized_request
+    rescue_from ExceptionHandler::AuthenticationError, with: :unauthorized_request
+  end
+
+  private
+
+  # JSON Response - 401 Unauthorized
+  def unauthorized_request(e)
+    err_response(e.message, :unauthorized)
+  end
+
+  # JSON Error Response - general method
+  def err_response(err, status)
+    json_response({ message: err.message}, status)
   end
 end
