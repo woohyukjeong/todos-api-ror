c11fca64d2bfdfbb2cdc9e27a57466e5bdf91864
woohyukjeong <woohyukjeong@dramancompany.com>
2024-06-03 15:14:30 +0900

    UserModel 설정 수정 - 비밀번호 암호화 적용 - 유효성 검증 추가 - 관계 정의

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,10 @@
 class User < ApplicationRecord
+  # 비밀번호 암호화
+  has_secure_password
+
+  # 관계정의
+  has_many :todos, foreign_key: :created_by
+
+  # 유효성 검증
+  validates_presence_of :name, :email, :password_digest
 end
