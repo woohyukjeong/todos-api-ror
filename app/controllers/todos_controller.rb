3db76a93baa822d0502d769a3756366a6130a54b
woohyukjeong <woohyukjeong@dramancompany.com>
2024-06-04 10:15:47 +0900

    Todo API 로직에 User 적용

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -2,7 +2,7 @@ class TodosController < ApplicationController
   before_action :set_todo, only: [:show, :update, :destroy]
 
   def index
-    @todos = Todo.all
+    @todos = current_user.todos
     json_response(@todos)
   end
 
@@ -21,7 +21,7 @@ class TodosController < ApplicationController
   end
 
   def create
-    @todo = Todo.create!(todo_params)
+    @todo = current_user.todos.create!(todo_params)
     json_response(@todo, :created)
   end
 
@@ -30,12 +30,12 @@ class TodosController < ApplicationController
 
   # Parameter 유효성 검증을 위한 메서드
   def todo_params
-    # Strong Parameters 기능을 활용하여 title과 created_by 필드만 요청의 매개변수로 허용
+    # Strong Parameters 기능을 활용하여 title  필드만 요청의 매개변수로 허용
     # 허용되지 않는 매개변수는 제거된다
-    params.permit(:title, :created_by)
+    params.permit(:title)
   end
 
   def set_todo
-    @todo = Todo.find(params[:id])
+    @todo = current_user.todos.find(params[:id])
   end
 end
